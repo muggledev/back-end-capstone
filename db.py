1e809f702b47d8eeb6f615c7a647f117bc715778
Rachelle Hooley <nrhooley@gmail.com>
2025-12-17 10:06:43 -0700

    Fixed db.py

--- db.py
+++ db.py
@@ -1,6 +1,14 @@
+from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+
+__all__ = ('db', 'init_db')
 
 db = SQLAlchemy()
 
-def init_db(app, database):
-    database.init_app(app)
+
+def init_db(app=None, db=None):
+    if isinstance(app, Flask) and isinstance(db, SQLAlchemy):
+        db.init_app(app)
+
+    else:
+        raise ValueError('cannot init db without db and app objects')
